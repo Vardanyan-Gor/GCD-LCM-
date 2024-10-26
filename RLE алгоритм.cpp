e7cca30ef0dc0c0f0348963bd528597cab66afda
Vardanyan-Gor <Gorv0301@gmail.com>
2024-10-27 00:47:21 +0400

    Add files via upload

---
+++ RLE алгоритм.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+std::string RLE(const std::string& input) {
+    std::string output;
+    int count = 1;
+
+    for (size_t i = 1; i <= input.length(); ++i) {
+        
+        if (i < input.length() && input[i] == input[i - 1]) {
+            count++;
+        }
+        else {
+            
+            output += std::to_string(count) + input[i - 1];
+            count = 1; 
+        }
+    }
+    return output;
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    std::string input;
+    std::cout << "Введите строку для сжатия: ";
+    std::getline(std::cin, input);
+
+    std::string compressed = RLE(input);
+    std::cout << "Сжатая строка (RLE): " << compressed << std::endl;
+
+    return 0;
+}
