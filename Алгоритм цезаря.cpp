708ad634d148a7d69051b884b9775c83e2744822
Vardanyan-Gor <Gorv0301@gmail.com>
2024-10-27 00:51:19 +0400

    Add files via upload

---
+++ Алгоритм цезаря.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+std::string caesarCipher(const std::string& text, int shift) {
+    std::string result;
+
+    for (char c : text) {
+        if (isalpha(c)) {
+            char base = islower(c) ? 'a' : 'A';
+            // Сдвиг и обеспечение цикличности алфавита
+            c = (c - base + shift) % 26 + base;
+        }
+        result += c; // Добавление символа к результату
+    }
+    return result;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    std::string text;
+    int shift;
+
+    std::cout << "Введите текст для шифрования: ";
+    std::getline(std::cin, text);
+    std::cout << "Введите сдвиг (число): ";
+    std::cin >> shift;
+
+    std::string encryptedText = caesarCipher(text, shift);
+    std::cout << "Зашифрованный текст: " << encryptedText << std::endl;
+
+    return 0;
+}
