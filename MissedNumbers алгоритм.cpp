778e25c9fbc1fbf0d3a99a86977bbc03affa22ec
Vardanyan-Gor <Gorv0301@gmail.com>
2024-10-26 19:33:37 +0400

    Add files via upload

---
+++ MissedNumbers алгоритм.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int missingNumber(const vector<int>& nums) {
+    int n = nums.size();
+    int expectedSum = n * (n + 1) / 2;
+    int actualSum = 0;
+
+    for (int num : nums) {
+        actualSum += num;
+    }
+
+    return expectedSum - actualSum; 
+}
+
+int main() {
+    setlocale( LC_ALL, "Rus");
+    vector<int> nums = { 3, 0, 1, 4,5,6 }; 
+    cout << "Пропущенное число: " << missingNumber(nums) << endl;  
+    return 0;
+}
