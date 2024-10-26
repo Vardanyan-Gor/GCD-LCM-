c9b7d0a0057884906942843f6651035c61075b3f
Vardanyan-Gor <Gorv0301@gmail.com>
2024-10-26 18:35:09 +0400

    Add files via upload

---
+++ GCD и LCM алгоритм.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int GCD (int x, int y) {
+    while (y != 0) {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+    return x;
+}
+int LCM (int x, int y) {
+    return (x * y) / GCD(x, y);
+}
+int main() {
+    int num1 = 48, num2 = 60;
+    cout << "GCD: " << GCD(num1, num2) << endl;
+    cout << "LCM: " << LCM(num1, num2) << endl;
+    return 0;
+}
